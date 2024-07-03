02b079762d4d23f3373f234566e11aeea5dad009
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-03 13:21:36 +0300

    completed the functions lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+//function declaration
+function saturdayFun (activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+//function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+//scope chain
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
